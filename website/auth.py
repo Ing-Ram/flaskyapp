41dfb343cf0463de324c209c50ccc61f91088195
Chad Ingram <Chad117gram@gmail.com>
2022-07-20 13:23:18 -0700

    finished templates and added database functionality

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,46 @@
-from flask import Blueprint
-
+from crypt import methods
+from hashlib import sha256
+from flask import Blueprint, render_template, request, flash, redirect,url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return '<p>Login</p>'
+    data = str(request.form)
+    print(data)
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET','POST'])
 def sign_up():
-    return '<p>Sign Up</p>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Email must be less than 7 characters', category='error')
+        else:
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account Created', category='success')
+            return redirect(url_for('views.home'))
+    return render_template("sign_up.html")
+
+    '''
+        user: chad@gmail.com 
+        password: lesson1
+    '''
